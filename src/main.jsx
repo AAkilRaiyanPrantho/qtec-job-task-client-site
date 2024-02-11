84f349b0675d60e32af587498f7fa87de86f83fa
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2024-02-11 22:05:03 +0600

    Headings component added

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,29 @@ import {
 } from "react-router-dom";
 import './index.css'
 import Main from './Layout/Main';
+import ErrorPage from './Pages/Error/ErrorPage';
+import Home from './Pages/Home/Home';
 
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      }
+      // {
+      //   path: "/signUp",
+      //   element: <SignUp></SignUp>
+      // },
+      // {
+      //   path: "/signIn",
+      //   element: <SignIn></SignIn>
+      // }
+    ]
   },
 ]);
 
